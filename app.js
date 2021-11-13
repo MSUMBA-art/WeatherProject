a7040ca75d9509eeb82ccdaf38274c64069b6534
ALEXANDER <msumba83@gmail.com>
2021-11-13 21:45:34 +0200

    how to parse Json

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const app = express();
 
 app.get("/", function(req, res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=moscow&units=metric&appid=cab38482ff42bfe2e937fb9afa90c848";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=cab38482ff42bfe2e937fb9afa90c848";
     https.get(url, function(response) {
-        console.log(response);
+        console.log(response.statusCode);
+
+         response.on("data", function(data) {
+        const weatherData = JSON.parse(data)
+        const temp = weatherData.main.temp
+        const weatherDescription = weatherData.weather[0].description
+           console.log(weatherDescription);
+         })
     })
 
 
