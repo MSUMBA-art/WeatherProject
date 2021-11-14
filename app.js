f933bcd3e816db3c7db331eb7bf76c5123e16ea9
ALEXANDER <msumba83@gmail.com>
2021-11-14 21:12:01 +0200

    using body parser in json

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=cab38482ff42bfe2e937fb9afa90c848";
-    https.get(url, function(response) {
-        console.log(response.statusCode);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-         response.on("data", function(data) {
-        const weatherData = JSON.parse(data)
-        const temp = weatherData.main.temp
-        const weatherDescription = weatherData.weather[0].description
-        const icon = weatherData.weather[0].icon
-        const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-        res.write("<p>The weather is currently " + weatherDescription + "." + "</p>");
-        res.write("<h1> The temperature in Cape Town is " + temp + " Degress Celcius.</h1>");
-        res.write("<img src=" + imageUrl + ">")
-        res.send()
-         })
-    })
-   
-})
+app.post("/", function (req, res) {
+
+    console.log(req.body.cityName);
+
+    // const query = "London";
+//     const apiKey = "cab38482ff42bfe2e937fb9afa90c848";
+//     const unit = "metric";
+//     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=metric";
+//     https.get(url, function(response) {
+//         console.log(response.statusCode);
+
+//          response.on("data", function(data) {
+//         const weatherData = JSON.parse(data)
+//         const temp = weatherData.main.temp
+//         const weatherDescription = weatherData.weather[0].description
+//         const icon = weatherData.weather[0].icon
+//         const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+//         res.write("<p>The weather is currently " + weatherDescription + "." + "</p>");
+//         res.write("<h1> The temperature in Cape Town is " + temp + " Degress Celcius.</h1>");
+//         res.write("<img src=" + imageUrl + ">")
+//         res.send()
+//          });
+//     });
+  
+});
 
 
 
-
-
-app.listen(3000, function() {
-   console.log("Server is listening on port 3000")
-})
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000");
+});
